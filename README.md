5a796c4d712f068d2f0bd922caf5f5c35eccc35f
jiangWeixian <jiangweixian1994@gmail.com>
2022-02-19 20:49:24 +0800

    :tada:  stitches config

---
+++ README.md
@@ -0,0 +1,38 @@
+# stitches.config
+*my stitches config*
+
+## features
+
+[Stitches](https://stitches.dev/) config of popular design system like `tailwindcss` and `macos`
+
+### themes
+
+- [tailwindcss](https://github.com/JiangWeixian/tailwindcss) - stitches with 🎐 [tailwindcss](https://tailwindcss.com/)
+- [(WIP)macos@monterey](https://github.com/JiangWeixian/macos) - stitches with 🍎 [macos](https://developer.apple.com/design/human-interface-guidelines/macos/visual-design/color/) style guide
+  - system dark
+  - [ ] system light 
+
+### utils
+
+- Alias for margin e.g. `my/mx/mt/mb`
+- Alias for padding e.g. `py/px/pt/pb`
+- Size for combine `width/height`
+- Text for combine `fontSize/lineHeight`
+  
+see more details in config file
+
+## usage
+
+Copy or move dir e.g. `tailwindcss` directly to project.
+
+## development
+
+- **Setup** - `pnpm i`
+- **Build** - `pnpm build`
+
+# 
+<div align='right'>
+
+*built with ❤️ by 😼*
+
+</div>
---
+++ macos/colors.dark.ts
@@ -0,0 +1,63 @@
+/**
+ * Monterey
+ * v12.1
+ * @see {@link https://developer.apple.com/design/human-interface-guidelines/macos/visual-design/color/}
+ */
+
+export const colors = {
+  // macos dev color palette - system main color
+  blue: '#106bff',
+  read: '#fc2b2d',
+  yellow: '#fecf0f',
+  green: '#30d33b',
+  gray: 'rgb(152,152,157)',
+  cyan: '#4cbcf2',
+
+  white: 'rgb(255 255 255)',
+  black: 'rgb(0 0 0)',
+  current: 'currentColor',
+
+  // mac dark theme color preset
+  labelColor: '#dddddd',
+  secondaryLabelColor: '#999999',
+  tertiaryLabelColor: '#555555',
+  quaternaryLabelColor: '#333333',
+  textColor: '$white',
+  textBackgroundColor: '#1d1d1d',
+  linkColor: '#3586ff',
+  placeholderTextColor: '$tertiaryLabelColor',
+  selectedTextColor: '$white',
+  selectedTextBackgroundColor: '#314f78',
+  keyboardFocusIndicatorColor: '#1c96ff',
+  unemphasizedSelectedTextColor: '$white',
+  unemphasizedSelectedTextBackgroundColor: '#363636',
+  separatorColor: '$quaternaryLabelColor',
+  unemphasizedSelectedContentBackgroundColor: '$unemphasizedSelectedTextBackgroundColor',
+  selectedMenuItemTextColor: '$white',
+  gridColor: '#141414',
+  headerTextColor: '$white',
+  alternateSelectedControlTextColor: '$white',
+  alternatingContentBackgroundColor0: '$textBackgroundColor',
+  alternatingContentBackgroundColor1: '#282828',
+  controlAccentColor: '#0a60ff',
+  controlColor: '$tertiaryLabelColor',
+  controlBackgroundColor: '$textBackgroundColor',
+  controlTextColor: '#d9d9d9',
+  disabledControlTextColor: '#606060',
+  selectedControlColor: '#314f78',
+  selectedControlTextColor: '$white',
+  windowBackgroundColor: '#262626',
+  windowFrameTextColor: '$white',
+  underPageBackgroundColor: '#1e1e1e',
+  findHighlightColor: '#ffff0b',
+  highlightColor: '#b4b4b4',
+  shadowColor: '$black',
+
+  // rename
+  primary: '$blue',
+  danger: '$red',
+  warning: '$yellow',
+  success: '$green',
+  default: '$gray',
+  info: '$cyan',
+}
